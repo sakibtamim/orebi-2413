c82fcc78435cc6d63fb049ddffd59ed012b79792
sakibtamim <sakib.tamim@gmail.com>
2025-07-11 22:02:36 +0600

    add Home, Header, and Container components; update App to use Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello</h1>
+      <Home />
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "./Container";
+
+const Header = () => {
+  return (
+    <header>
+      <Container>
+        <h2>hellio</h2>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
