229e2f2d027568c15bb5607db056b47fac0cb014
sakibtamim <sakib.tamim@gmail.com>
2025-07-22 12:54:10 +0600

    feat: implement routing with react-router-dom; add Layout and Error404 components; update Home and Shop pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import {
+  createBrowserRouter,
+  createRoutesFromChildren,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/Home";
+import Layout from "./components/Layout";
+import Shop from "./pages/Shop";
+import Error404 from "./components/Error404";
+let router = createBrowserRouter(
+  createRoutesFromChildren(
+    <>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
+      </Route>
+      <Route path="*" element={<Error404 />}></Route>
+    </>
+  )
+);
 
 function App() {
   return (
     <>
-      <Home />
+      <RouterProvider router={router}></RouterProvider>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,20 @@
 import React from "react";
-import Header from "../components/Header";
-import Navbar from "../components/Navbar";
 import Banner from "../components/Banner";
 import Ads from "../components/Ads";
 import NewArrivals from "../components/NewArrivals";
 import BestSellers from "../components/BestSellers";
 import AdsBtm from "../components/AdsBtm";
 import SpecialOffers from "../components/SpecialOffers";
-import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
-      <Header />
-      <Navbar />
       <Banner />
       <Ads />
       <NewArrivals />
       <BestSellers />
       <AdsBtm />
       <SpecialOffers />
-      <Footer />
     </>
   );
 };
