e962eaea81cce6b87aff2350f214c6971f82de50
sakibtamim <sakib.tamim@gmail.com>
2025-07-29 04:33:35 +0600

    feat: add loading state to BestSellers, NewArrivals, and SpecialOffers components; implement loading indicators during data fetching

--- src/components/BestSellers.jsx
+++ src/components/BestSellers.jsx
@@ -1,11 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Container from "./Container";
 import { FaHeart, FaShoppingCart } from "react-icons/fa";
 import { IoGitCompare } from "react-icons/io5";
 import { ApiData } from "./ContextApi";
 
 const BestSellers = () => {
-  let data = useContext(ApiData);
+  let { data, loading } = useContext(ApiData);
+
+  let [showLoading, setShowLoading] = useState(true);
+
+  useEffect(() => {
+    if (!loading) {
+      const timer = setTimeout(() => {
+        setShowLoading(false);
+      }, 1000);
+      return () => clearTimeout(timer);
+    } else {
+      setShowLoading(true);
+    }
+  }, [loading]);
 
   return (
     <section className="lg:pt-[118px] pt-[40px] lg:pb-[130px] pb-[40px]">
@@ -15,66 +28,73 @@ const BestSellers = () => {
             Best Sellers
           </h2>
         </div>
-        <div className="flex justify-between flex-wrap">
-          {data.slice(20, 24).map((item) => (
-            <div key={item.id} className="lg:w-[24%] w-[48%]">
-              <div className="relative group">
-                <div className="bg-[#F9F9F9]">
-                  <img src={item.thumbnail} alt="" />
+
+        {showLoading ? (
+          <div className="w-full h-50  flex justify-center items-center ">
+            <div className=" w-16 h-16 border-6 border-primary border-t-transparent  rounded-full animate-spin"></div>
+          </div>
+        ) : (
+          <div className="flex justify-between flex-wrap">
+            {data.slice(20, 24).map((item) => (
+              <div key={item.id} className="lg:w-[24%] w-[48%]">
+                <div className="relative group">
+                  <div className="bg-[#F9F9F9]">
+                    <img src={item.thumbnail} alt="" />
+                  </div>
+                  <div className="absolute left-0 bottom-0 w-full bg-white scale-0 group-hover:scale-100 duration-300 ease-in-out origin-bottom-left">
+                    <ul className=" py-[25px] pr-[30px]">
+                      <li>
+                        <a
+                          href=""
+                          className="flex items-center justify-end gap-x-[16px] text-secondary text-[16px] font-dmsans font-normal hover:text-primary hover:font-bold"
+                        >
+                          <span>Add to Wish List</span>
+                          <FaHeart className="text-primary " />
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href=""
+                          className="flex items-center justify-end gap-x-[16px] py-[20px]  text-secondary text-[16px] font-dmsans font-normal hover:text-primary hover:font-bold"
+                        >
+                          <span>Compare</span>
+                          <IoGitCompare className="text-primary " />
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href=""
+                          className="flex items-center justify-end gap-x-[16px]  text-secondary text-[16px] font-dmsans font-normal hover:text-primary hover:font-bold"
+                        >
+                          <span>Add to Cart</span>
+                          <FaShoppingCart className="text-primary " />
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="absolute lg:top-[20px] top-[10px] lg:left-[20px] left-[10px] lg:px-[32px] px-[14px] lg:py-[8px] py-[6px] bg-primary lg:text-[14px] text-[10px] text-white lg:font-bold font-semibold font-dmsans">
+                    {item.discountPercentage}%
+                  </div>
                 </div>
-                <div className="absolute left-0 bottom-0 w-full bg-white scale-0 group-hover:scale-100 duration-300 ease-in-out origin-bottom-left">
-                  <ul className=" py-[25px] pr-[30px]">
-                    <li>
-                      <a
-                        href=""
-                        className="flex items-center justify-end gap-x-[16px] text-secondary text-[16px] font-dmsans font-normal hover:text-primary hover:font-bold"
-                      >
-                        <span>Add to Wish List</span>
-                        <FaHeart className="text-primary " />
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href=""
-                        className="flex items-center justify-end gap-x-[16px] py-[20px]  text-secondary text-[16px] font-dmsans font-normal hover:text-primary hover:font-bold"
-                      >
-                        <span>Compare</span>
-                        <IoGitCompare className="text-primary " />
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href=""
-                        className="flex items-center justify-end gap-x-[16px]  text-secondary text-[16px] font-dmsans font-normal hover:text-primary hover:font-bold"
-                      >
-                        <span>Add to Cart</span>
-                        <FaShoppingCart className="text-primary " />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <div className="absolute lg:top-[20px] top-[10px] lg:left-[20px] left-[10px] lg:px-[32px] px-[14px] lg:py-[8px] py-[6px] bg-primary lg:text-[14px] text-[10px] text-white lg:font-bold font-semibold font-dmsans">
-                  {item.discountPercentage}%
+                <div className="">
+                  <div className="flex justify-between items-center pt-[24px] pb-[15px]">
+                    <h3 className="lg:text-[18px] text-[14px] text-primary font-bold font-dmsans">
+                      {item.title}
+                    </h3>
+                    <p className="lg:text-[16px] text-[12px] text-secondary font-normal font-dmsans">
+                      ${item.price}
+                    </p>
+                  </div>
+                  <div>
+                    <p className="lg:text-[16px] text-[12px] text-secondary font-normal font-dmsans capitalize">
+                      {item.category}
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div className="">
-                <div className="flex justify-between items-center pt-[24px] pb-[15px]">
-                  <h3 className="lg:text-[18px] text-[14px] text-primary font-bold font-dmsans">
-                    {item.title}
-                  </h3>
-                  <p className="lg:text-[16px] text-[12px] text-secondary font-normal font-dmsans">
-                    ${item.price}
-                  </p>
-                </div>
-                <div>
-                  <p className="lg:text-[16px] text-[12px] text-secondary font-normal font-dmsans capitalize">
-                    {item.category}
-                  </p>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </Container>
     </section>
   );
--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Container from "./Container";
 import {
   FaHeart,
@@ -29,7 +29,20 @@ function ArrivalPrevArrow(props) {
 }
 
 const NewArrivals = () => {
-  let data = useContext(ApiData);
+  let { data, loading } = useContext(ApiData);
+
+  let [showLoading, setShowLoading] = useState(true);
+
+  useEffect(() => {
+    if (!loading) {
+      const timer = setTimeout(() => {
+        setShowLoading(false);
+      }, 1000);
+      return () => clearTimeout(timer);
+    } else {
+      setShowLoading(true);
+    }
+  }, [loading]);
 
   let ArrivalSlider = {
     infinite: true,
@@ -65,66 +78,72 @@ const NewArrivals = () => {
           </h2>
         </div>
         <div>
-          <Slider {...ArrivalSlider}>
-            {data.map((item) => (
-              <div key={item.id} className="!w-[95%]">
-                <div className="relative group">
-                  <div className="bg-[#F9F9F9]">
-                    <img src={item.thumbnail} alt="" />
+          {showLoading ? (
+            <div className="w-full h-50  flex justify-center items-center ">
+              <div className=" w-16 h-16 border-6 border-primary border-t-transparent  rounded-full animate-spin"></div>
+            </div>
+          ) : (
+            <Slider {...ArrivalSlider}>
+              {data.map((item) => (
+                <div key={item.id} className="!w-[95%]">
+                  <div className="relative group">
+                    <div className="bg-[#F9F9F9]">
+                      <img src={item.thumbnail} alt="" />
+                    </div>
+                    <div className="absolute left-0 bottom-0 w-full bg-white scale-0 group-hover:scale-100 duration-300 ease-in-out origin-bottom-left">
+                      <ul className=" py-[25px] pr-[30px]">
+                        <li>
+                          <a
+                            href=""
+                            className="flex items-center justify-end gap-x-[16px] text-secondary text-[16px] font-dmsans font-normal hover:text-primary hover:font-bold"
+                          >
+                            <span>Add to Wish List</span>
+                            <FaHeart className="text-primary " />
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href=""
+                            className="flex items-center justify-end gap-x-[16px] py-[20px]  text-secondary text-[16px] font-dmsans font-normal hover:text-primary hover:font-bold"
+                          >
+                            <span>Compare</span>
+                            <IoGitCompare className="text-primary " />
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href=""
+                            className="flex items-center justify-end gap-x-[16px]  text-secondary text-[16px] font-dmsans font-normal hover:text-primary hover:font-bold"
+                          >
+                            <span>Add to Cart</span>
+                            <FaShoppingCart className="text-primary " />
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="absolute lg:top-[20px] top-[10px] lg:left-[20px] left-[10px] lg:px-[32px] px-[14px] lg:py-[8px] py-[6px] bg-primary lg:text-[14px] text-[10px] text-white lg:font-bold font-semibold font-dmsans">
+                      {item.discountPercentage}%
+                    </div>
                   </div>
-                  <div className="absolute left-0 bottom-0 w-full bg-white scale-0 group-hover:scale-100 duration-300 ease-in-out origin-bottom-left">
-                    <ul className=" py-[25px] pr-[30px]">
-                      <li>
-                        <a
-                          href=""
-                          className="flex items-center justify-end gap-x-[16px] text-secondary text-[16px] font-dmsans font-normal hover:text-primary hover:font-bold"
-                        >
-                          <span>Add to Wish List</span>
-                          <FaHeart className="text-primary " />
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href=""
-                          className="flex items-center justify-end gap-x-[16px] py-[20px]  text-secondary text-[16px] font-dmsans font-normal hover:text-primary hover:font-bold"
-                        >
-                          <span>Compare</span>
-                          <IoGitCompare className="text-primary " />
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href=""
-                          className="flex items-center justify-end gap-x-[16px]  text-secondary text-[16px] font-dmsans font-normal hover:text-primary hover:font-bold"
-                        >
-                          <span>Add to Cart</span>
-                          <FaShoppingCart className="text-primary " />
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="absolute lg:top-[20px] top-[10px] lg:left-[20px] left-[10px] lg:px-[32px] px-[14px] lg:py-[8px] py-[6px] bg-primary lg:text-[14px] text-[10px] text-white lg:font-bold font-semibold font-dmsans">
-                    {item.discountPercentage}%
+                  <div className="">
+                    <div className="flex justify-between items-center pt-[24px] pb-[15px]">
+                      <h3 className="lg:text-[18px] text-[14px] text-primary font-bold font-dmsans">
+                        {item.title}
+                      </h3>
+                      <p className="lg:text-[16px] text-[12px] text-secondary font-normal font-dmsans">
+                        ${item.price}
+                      </p>
+                    </div>
+                    <div>
+                      <p className="lg:text-[16px] text-[12px] text-secondary font-normal font-dmsans capitalize">
+                        {item.category}
+                      </p>
+                    </div>
                   </div>
                 </div>
-                <div className="">
-                  <div className="flex justify-between items-center pt-[24px] pb-[15px]">
-                    <h3 className="lg:text-[18px] text-[14px] text-primary font-bold font-dmsans">
-                      {item.title}
-                    </h3>
-                    <p className="lg:text-[16px] text-[12px] text-secondary font-normal font-dmsans">
-                      ${item.price}
-                    </p>
-                  </div>
-                  <div>
-                    <p className="lg:text-[16px] text-[12px] text-secondary font-normal font-dmsans capitalize">
-                      {item.category}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </Slider>
+              ))}
+            </Slider>
+          )}
         </div>
       </Container>
     </section>
