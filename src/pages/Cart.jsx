c2e26b6b56b1a97bda0adcc4d70da509baa303d1
sakibtamim <sakib.tamim@gmail.com>
2025-08-19 23:06:09 +0600

    feat: enhance Cart component with size selection, coupon application, and cart totals display

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -30,7 +30,7 @@ const Cart = () => {
           </div>
         </div>
         <div className="border border-[#F0F0F0]">
-          <div className="flex bg-[#F5F5F3] py-[34px] px-[20px]">
+          <div className="flex bg-[#F5F5F3] py-[34px] px-[20px] ">
             <div className="w-1/4 ">
               <h4 className="text-[16px] text-primary font-bold font-dmsans">
                 Product
@@ -52,7 +52,7 @@ const Cart = () => {
               </h4>
             </div>
           </div>
-          <div className="flex items-center py-[30px] px-[20px]">
+          <div className="flex items-center py-[30px] px-[20px] ">
             <div className="w-1/4 flex items-center">
               <div className="pr-[40px]">
                 <IoClose className="text-primary" />
@@ -88,7 +88,70 @@ const Cart = () => {
               </h4>
             </div>
           </div>
-          <div className=""></div>
+          <div className="flex  p-[20px] border-t-1 border-t-[#F0F0F0]">
+            <div className="w-1/2 flex items-center gap-x-[24px]">
+              <div className="">
+                <select
+                  name=""
+                  id=""
+                  className="w-[255px] py-[10px] px-[20px] text-[14px] text-secondary font-dmsans font-normal border  border-[#F0F0F0] outline-none"
+                >
+                  <option value="">SIZE</option>
+                  <option value="Big">Big</option>
+                  <option value="Small">Small</option>
+                </select>
+              </div>
+              <h5 className="text-[14px] text-primary font-dmsans font-bold">
+                Apply coupon
+              </h5>
+            </div>
+            <div className="w-1/2 text-end">
+              <h5 className="text-[14px] text-primary font-dmsans font-bold">
+                Update cart
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div className="pt-[54px] flex justify-end">
+          <div className="w-5/12">
+            <h3 className="text-[20px] text-primary text-end font-bold font-dmsans pb-[24px]">
+              Cart totals
+            </h3>
+            <div className=" border border-[#F0F0F0] mb-[30px]">
+              <div className="flex border-b-1 border-b-[#F0F0F0]">
+                <div className="w-1/2  border-r-1 border-r-[#F0F0F0]">
+                  <h4 className="py-[16px] pl-[20px] text-[16px] text-primary font-bold font-dmsans">
+                    Subtotal
+                  </h4>
+                </div>
+                <div className="w-1/2 ">
+                  <h4 className="py-[16px] pl-[20px] text-[16px] text-primary font-normal font-dmsans">
+                    389.99 $
+                  </h4>
+                </div>
+              </div>
+              <div className="flex ">
+                <div className="w-1/2  border-r-1 border-r-[#F0F0F0]">
+                  <h4 className="py-[16px] pl-[20px] text-[16px] text-primary font-bold font-dmsans">
+                    Total
+                  </h4>
+                </div>
+                <div className="w-1/2 ">
+                  <h4 className="py-[16px] pl-[20px] text-[16px] text-primary font-normal font-dmsans">
+                    389.99 $
+                  </h4>
+                </div>
+              </div>
+            </div>
+            <div className="text-end">
+              <a
+                href="#"
+                className=" text-[14px] text-white font-bold font-dmsans py-[16px] px-[28px] bg-primary inline-block"
+              >
+                Proceed to Checkout
+              </a>
+            </div>
+          </div>
         </div>
       </Container>
     </section>
