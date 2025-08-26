218af4bf8a3f64c4a3f6c80a90349402329202bd
sakibtamim <sakib.tamim@gmail.com>
2025-08-26 13:06:54 +0600

    feat: add product details page with API integration

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Container from "../components/Container";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  let productId = useParams();
+  let [productData, setProductData] = useState({});
+  let getProductDetailsData = () => {
+    axios
+      .get(`https://dummyjson.com/products/${productId.id}`)
+      .then((response) => {
+        setProductData(response.data);
+      });
+  };
+  useEffect(() => {
+    getProductDetailsData();
+  }, []);
+
+  return (
+    <section>
+      <Container>
+        <div className="">{productData.id}</div>
+        <div className="">
+          <img src={productData.thumbnail} alt="" />
+        </div>
+        <div className="">{productData.title}</div>
+        <div className="">{productData.description}</div>
+        <div className="">{productData.price} $</div>
+        <div className="">{productData.stock} pcs in stock</div>
+      </Container>
+    </section>
+  );
 };
 
 export default ProductDetails;
